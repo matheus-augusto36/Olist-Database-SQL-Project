642283afb3d35db62cb919964eae62bf950c4cdd
Matheus Augusto <silvamatheusaugusto36@gmail.com>
2025-12-16 22:03:01 -0300

    first commands and insights

--- Olist Project.sql
+++ Olist Project.sql
@@ -19,14 +19,92 @@ JOIN products p ON o.product_id = p.product_id
 group by p.product_category_name 
 order by total_orders DESC;
 
--- Quantity of orders by product category (only high score)
+-- Quantity of orders by product category (only low score)
 SELECT 
 		p.product_category_name as category,
         COUNT(DISTINCT oi.order_id) as total_orders
 FROM order_reviews orw 
 JOIN order_items oi ON orw.order_id = oi.order_id 
 JOIN products p ON oi.product_id = p.product_id 
-WHERE orw.review_score = 5.0
+WHERE orw.review_score < 5.0
 group by p.product_category_name 
 order by total_orders DESC;
+
+-- Percentage of low score orders by product category
+SELECT 
+		p.product_category_name as category,
+        ROUND(
+			COUNT(DISTINCT CASE WHEN orw.review_score < 4 THEN oi.order_id END) * 100.0 /
+            COUNT(DISTINCT oi.order_id)
+        , 2) AS low_score_percentage
+FROM order_reviews orw 
+JOIN order_items oi ON orw.order_id = oi.order_id 
+JOIN products p ON oi.product_id = p.product_id 
+group by p.product_category_name 
+order by low_score_percentage DESC;
+
+
+-- categories with 100% low score
+WITH orders_percentage AS (
+	SELECT p.product_category_name as category,
+		ROUND (
+			COUNT(DISTINCT CASE WHEN orw.review_score < 4 THEN oi.order_id END) * 100.0
+			/ COUNT(DISTINCT oi.order_id), 2
+		) as low_score_percentage
+	FROM products p 
+	JOIN order_items oi 
+	ON p.product_id = oi.product_id
+	JOIN order_reviews orw
+	ON orw.order_id = oi.order_id
+	GROUP BY p.product_category_name
+	ORDER BY low_score_percentage DESC
+)
+SELECT * 
+FROM orders_percentage
+WHERE low_score_percentage = 100.0;
+
+
+-- Orders by 'diff_delivery_time' -> difference between the shipping limit date and the date the product was delivered
+-- diff_delivery_time > 0 => the product was delivered on time
+-- diff_delivery_time = 0 => the product was delivered on time, but exactly on the limit date
+-- diff_delivery_time < 0 => the product was delivered late
+SELECT 
+	o.order_id,
+	oi.shipping_limit_date,
+	o.order_delivered_customer_date,
+	datediff(oi.shipping_limit_date, o.order_delivered_customer_date) as diff_delivery_time
+FROM orders o
+JOIN order_items oi
+ON o.order_id = oi.order_id
+ORDER BY diff_delivery_time ASC;
+
+-- average of 'diff_delivery_time' grouped by the product category
+SELECT
+	p.product_category_name,
+	round(avg(datediff(oi.shipping_limit_date, o.order_delivered_customer_date)), 1) as avg_delivery_time
+FROM orders o
+JOIN order_items oi
+ON o.order_id = oi.order_id
+JOIN products p
+ON oi.product_id = p.product_id
+GROUP BY p.product_category_name
+order by avg_delivery_time ASC;
+
+-- average of 'diff_delivery_time' grouped by the product category, but showing only the categories with 100% low score
+SELECT
+	p.product_category_name,
+	round(avg(datediff(oi.shipping_limit_date, o.order_delivered_customer_date)), 1) as avg_delivery_time
+FROM orders o
+JOIN order_items oi
+ON o.order_id = oi.order_id
+JOIN products p
+ON oi.product_id = p.product_id
+WHERE p.product_category_name IN ('alimentos_bebidas', 'artes', 'livros_tecnicos', 'malas_acessorios')
+GROUP BY p.product_category_name
+order by avg_delivery_time ASC;
+
+
+
+
+
 
